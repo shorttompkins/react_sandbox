d480f35d678e7f4038bf951b9391820732bf5f5c
Jason Krol <jkat98@gmail.com>
2015-09-24 17:00:16 -0400

    Introduce async with superagent

--- server/index.js
+++ server/index.js
@@ -3,4 +3,7 @@ var express = require('express');
 module.exports = function (app, config) {
 	// Route handling and middleware should go here
 	app.use(express.static('build'));
+  app.get('/api/data', function(req, res){
+    res.json({'success':true});
+  });
 };
